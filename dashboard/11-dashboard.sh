be76cc81869d0e33db4e1ff3180ddd6a19db6de0
smbek <smbek.kg@gmail.com>
2024-06-27 21:15:46 +0000

    updated

--- dashboard/11-dashboard.sh
+++ dashboard/11-dashboard.sh
@@ -9,3 +9,25 @@ sudo helm install kubernetes-dashboard kubernetes-dashboard/kubernetes-dashboard
 # sudo kubectl get svc
 # sudo kubectl get node -o wide 
 # sudo kubectl get pods
+# sudo kubectl get sa
+# sudo kubectl edit sa kubernetes-dashboard-kong 
+# secrets:
+#   - name: admin-token
+# vi secret.yaml
+# apiVersion: v1
+# kind: Secret
+# metadata:
+#   name: admin-token
+#   annotations:
+#     kubernetes.io/service-account.name: kubernetes-dashboard-kong
+# type: kubernetes.io/service-account-token
+
+# kubectl apply -f secret.yaml
+
+# kubectl get secret
+
+# kubectl create clusterrolebinding kubernetes-dashboard --clusterrole=cluster-admin --serviceaccount=default:kubernetes-dashboard-kong
+
+# kubectl get secret $(kubectl get sa kubernetes-dashboard-kong  -o jsonpath='{.secrets[0].name}') -o go-template='{{.data.token | base64decode}}'
+
+# 
